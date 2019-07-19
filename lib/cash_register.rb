96c290fa1c138ee5c01af9d251d1c0f732cab499
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-19 19:17:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,16 +5,12 @@ class CashRegister
 
   
  def initialize(total = 0.00, discount = 20)
-   
      @total = total
    @discount = discount 
- 
 end 
 
 def add_item(title, price, quantity = 1)
   self.total += price * quantity
-  
-  
 end
 
 
