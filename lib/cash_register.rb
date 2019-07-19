a220c4570d77631159ccde895fb8163d0e8e0c3d
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-19 19:42:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,21 +1,22 @@
+class CashRegister 
+  
+  attr_accessor :items, :discount, :total,
+  
 
-class CashRegister
+  
+ def initialize(discount=0)
+     @total = total
+   @discount = discount 
+   @items = []
+end 
 
-  attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
-    @total = 0
-    @discount = discount
-    @items = []
+def add_item(title, amount, quantity=1)
+  self.total += amount * quantity
+  quantity.times do
+    items << title
   end
-
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
-  end
+  self.last_transaction = amount * quantity
+end
 
 
   def apply_discount
